099a4f0be79da4794c7008bed6d22c28e6a249e0
Jagadeeswar JD <jagadeeswar.jd@gmail.com>
2021-04-10 16:28:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,341 @@
+let btn1=document.getElementById('btn1');
+let btn2=document.getElementById('btn2');
+let btn3=document.getElementById('btn3');
+let btn4=document.getElementById('btn4');
+let btn5=document.getElementById('btn5');
+let btn6=document.getElementById('btn6');
+let btn7=document.getElementById('btn7');
+let btn8=document.getElementById('btn8');
+let btn9=document.getElementById('btn9');
+let btn10=document.getElementById('btn10');
+let btn11=document.getElementById('btn11');
+let btn12=document.getElementById('btn12');
+let btn13=document.getElementById('btn13');
+let btn14=document.getElementById('btn14');
+let btn15=document.getElementById('btn15');
+let btn16=document.getElementById('btn16');
+let btn17=document.getElementById('btn17');
+let btn18=document.getElementById('btn18');
+let btn19=document.getElementById('btn19');
+let btn20=document.getElementById('btn20');
+let btn21=document.getElementById('btn21');
+let btn22=document.getElementById('btn22');
+let btn23=document.getElementById('btn23');
+let btn24=document.getElementById('btn24');
+let btn25=document.getElementById('btn25');
+let btn26=document.getElementById('btn26');
+let btn27=document.getElementById('btn27');
+let btn28=document.getElementById('btn28');
+let btn29=document.getElementById('btn29');
+let btn30=document.getElementById('btn30');
+
+let clear=document.getElementById('clear');
+
+clear.addEventListener('click',function(){
+    localStorage.clear();
+})
+btn1.addEventListener('click',function(){
+    btn1.textContent="Cart Added";
+    btn1.style.background="red";
+    let img1=document.getElementById('img1');
+    let med1=document.getElementById('med1');
+    let medp1=document.getElementById('medp1');
+    localStorage.setItem('img1',img1.src);
+    localStorage.setItem('med1',med1.textContent);
+    localStorage.setItem('medp1',medp1.textContent);
+    console.log(medp1.textContent);
+})
+
+btn2.addEventListener('click',function(){
+    btn2.textContent="Cart Added";
+    btn2.style.background="red";
+    let img2=document.getElementById('img2');
+    let med2=document.getElementById('med2');
+    let medp2=document.getElementById('medp2');
+    localStorage.setItem('img2',img2.src);
+    localStorage.setItem('med2',med2.textContent);
+    localStorage.setItem('medp2',medp2.textContent);
+})
+
+btn3.addEventListener('click',function(){
+    btn3.textContent="Cart Added";
+    btn3.style.background="red";
+    let img3=document.getElementById('img3');
+    let med3=document.getElementById('med3');
+    let medp3=document.getElementById('medp3');
+    localStorage.setItem('img3',img3.src);
+    localStorage.setItem('med3',med3.textContent);
+    localStorage.setItem('medp3',medp3.textContent);
+})
+btn4.addEventListener('click',function(){
+    btn4.textContent="Cart Added";
+    btn4.style.background="red";
+    let img4=document.getElementById('img4');
+    let med4=document.getElementById('med4');
+    let medp4=document.getElementById('medp4');
+    localStorage.setItem('img4',img4.src);
+    localStorage.setItem('med4',med4.textContent);
+    localStorage.setItem('medp4',medp4.textContent);
+})
+btn5.addEventListener('click',function(){
+    btn5.textContent="Cart Added";
+    btn5.style.background="red";
+    let img5=document.getElementById('img5');
+    let med5=document.getElementById('med5');
+    let medp5=document.getElementById('medp5');
+    localStorage.setItem('img5',img5.src);
+    localStorage.setItem('med5',med5.textContent);
+    localStorage.setItem('medp5',medp5.textContent);
+})
+btn6.addEventListener('click',function(){
+    btn6.textContent="Cart Added";
+    btn6.style.background="red";
+    let img6=document.getElementById('img6');
+    let med6=document.getElementById('med6');
+    let medp6=document.getElementById('medp6');
+    localStorage.setItem('img6',img6.src);
+    localStorage.setItem('med6',med6.textContent);
+    localStorage.setItem('medp6',medp6.textContent);
+})
+btn7.addEventListener('click',function(){
+    btn7.textContent="Cart Added";
+    btn7.style.background="red";
+    let img7=document.getElementById('img7');
+    let med7=document.getElementById('med7');
+    let medp7=document.getElementById('medp7');
+    localStorage.setItem('img7',img7.src);
+    localStorage.setItem('med7',med7.textContent);
+    localStorage.setItem('medp7',medp7.textContent);
+})
+btn8.addEventListener('click',function(){
+    btn8.textContent="Cart Added";
+    btn8.style.background="red";
+    let img8=document.getElementById('img8');
+    let med8=document.getElementById('med8');
+    let medp8=document.getElementById('medp8');
+    localStorage.setItem('img8',img8.src);
+    localStorage.setItem('med8',med8.textContent);
+    localStorage.setItem('medp8',medp8.textContent);
+})
+btn9.addEventListener('click',function(){
+    btn9.textContent="Cart Added";
+    btn9.style.background="red";
+    let img9=document.getElementById('img9');
+    let med9=document.getElementById('med9');
+    let medp9=document.getElementById('medp9');
+    localStorage.setItem('img9',img9.src);
+    localStorage.setItem('med9',med9.textContent);
+    localStorage.setItem('medp9',medp9.textContent);
+})
+btn10.addEventListener('click',function(){
+    btn10.textContent="Cart Added";
+    btn10.style.background="red";
+    let img10=document.getElementById('img10');
+    let med10=document.getElementById('med10');
+    let medp10=document.getElementById('medp10');
+    localStorage.setItem('img10',img10.src);
+    localStorage.setItem('med10',med10.textContent);
+    localStorage.setItem('medp10',medp10.textContent);
+})
+btn11.addEventListener('click',function(){
+    btn11.textContent="Cart Added";
+    btn11.style.background="red";
+    let img11=document.getElementById('img11');
+    let med11=document.getElementById('med11');
+    let medp11=document.getElementById('medp11');
+    localStorage.setItem('img11',img11.src);
+    localStorage.setItem('med11',med11.textContent);
+    localStorage.setItem('medp11',medp11.textContent);
+})
+btn12.addEventListener('click',function(){
+    btn12.textContent="Cart Added";
+    btn12.style.background="red";
+    let img12=document.getElementById('img12');
+    let med12=document.getElementById('med12');
+    let medp12=document.getElementById('medp12');
+    localStorage.setItem('img12',img12.src);
+    localStorage.setItem('med12',med12.textContent);
+    localStorage.setItem('medp12',medp12.textContent);
+})
+btn13.addEventListener('click',function(){
+    btn13.textContent="Cart Added";
+    btn13.style.background="red";
+    let img13=document.getElementById('img13');
+    let med13=document.getElementById('med13');
+    let medp13=document.getElementById('medp13');
+    localStorage.setItem('img13',img13.src);
+    localStorage.setItem('med13',med13.textContent);
+    localStorage.setItem('medp13',medp13.textContent);
+})
+btn14.addEventListener('click',function(){
+    btn14.textContent="Cart Added";
+    btn14.style.background="red";
+    let img14=document.getElementById('img14');
+    let med14=document.getElementById('med14');
+    let medp14=document.getElementById('medp14');
+    localStorage.setItem('img14',img14.src);
+    localStorage.setItem('med14',med14.textContent);
+    localStorage.setItem('medp14',medp14.textContent);
+
+})
+btn15.addEventListener('click',function(){
+    btn15.textContent="Cart Added";
+    btn15.style.background="red";
+    let img15=document.getElementById('img15');
+    let med15=document.getElementById('med15');
+    let medp15=document.getElementById('medp15');
+    localStorage.setItem('img15',img15.src);
+    localStorage.setItem('med15',med15.textContent);
+    localStorage.setItem('medp15',medp15.textContent);
+})
+btn16.addEventListener('click',function(){
+    btn16.textContent="Cart Added";
+    btn16.style.background="red";
+    let img16=document.getElementById('img16');
+    let med16=document.getElementById('med16');
+    let medp16=document.getElementById('medp16');
+    localStorage.setItem('img16',img16.src);
+    localStorage.setItem('med16',med16.textContent);
+    localStorage.setItem('medp16',medp16.textContent);
+})
+btn17.addEventListener('click',function(){
+    btn17.textContent="Cart Added";
+    btn17.style.background="red";
+    let img17=document.getElementById('img17');
+    let med17=document.getElementById('med17');
+    let medp17=document.getElementById('medp17');
+    localStorage.setItem('img17',img17.src);
+    localStorage.setItem('med17',med17.textContent);
+    localStorage.setItem('medp17',medp17.textContent);
+
+})
+btn18.addEventListener('click',function(){
+    btn18.textContent="Cart Added";
+    btn18.style.background="red";
+    let img18=document.getElementById('img18');
+    let med18=document.getElementById('med18');
+    let medp18=document.getElementById('medp18');
+    localStorage.setItem('img18',img18.src);
+    localStorage.setItem('med18',med18.textContent);
+    localStorage.setItem('medp18',medp18.textContent);
+})
+btn19.addEventListener('click',function(){
+    btn19.textContent="Cart Added";
+    btn19.style.background="red";
+    let img19=document.getElementById('img19');
+    let med19=document.getElementById('med19');
+    let medp19=document.getElementById('medp19');
+    localStorage.setItem('img19',img19.src);
+    localStorage.setItem('med19',med19.textContent);
+    localStorage.setItem('medp1',medp19.textContent);
+})
+btn20.addEventListener('click',function(){
+    btn20.textContent="Cart Added";
+    btn20.style.background="red";
+    let img20=document.getElementById('img20');
+    let med20=document.getElementById('med20');
+    let medp20=document.getElementById('medp20');
+    localStorage.setItem('img20',img20.src);
+    localStorage.setItem('med20',med20.textContent);
+    localStorage.setItem('medp20',medp20.textContent);
+})
+btn21.addEventListener('click',function(){
+    btn21.textContent="Cart Added";
+    btn21.style.background="red";
+    let img21=document.getElementById('img21');
+    let med21=document.getElementById('med21');
+    let medp21=document.getElementById('medp21');
+    localStorage.setItem('img21',img21.src);
+    localStorage.setItem('med21',med21.textContent);
+    localStorage.setItem('medp21',medp21.textContent);
+})
+btn22.addEventListener('click',function(){
+    btn22.textContent="Cart Added";
+    btn22.style.background="red";
+    let img22=document.getElementById('img22');
+    let med22=document.getElementById('med22');
+    let medp22=document.getElementById('medp22');
+    localStorage.setItem('img22',img22.src);
+    localStorage.setItem('med22',med22.textContent);
+    localStorage.setItem('medp22',medp22.textContent);
+})
+btn23.addEventListener('click',function(){
+    btn23.textContent="Cart Added";
+    btn23.style.background="red";
+    let img23=document.getElementById('img23');
+    let med23=document.getElementById('med23');
+    let medp23=document.getElementById('medp23');
+    localStorage.setItem('img23',img23.src);
+    localStorage.setItem('med23',med23.textContent);
+    localStorage.setItem('medp23',medp23.textContent);
+})
+btn24.addEventListener('click',function(){
+    btn24.textContent="Cart Added";
+    btn24.style.background="red";
+    let img24=document.getElementById('img24');
+    let med24=document.getElementById('med24');
+    let medp24=document.getElementById('medp24');
+    localStorage.setItem('img24',img24.src);
+    localStorage.setItem('med24',med24.textContent);
+    localStorage.setItem('medp24',medp24.textContent);
+})
+btn25.addEventListener('click',function(){
+    btn25.textContent="Cart Added";
+    btn25.style.background="red";
+    let img25=document.getElementById('img25');
+    let med25=document.getElementById('med25');
+    let medp25=document.getElementById('medp25');
+    localStorage.setItem('img25',img25.src);
+    localStorage.setItem('med25',med25.textContent);
+    localStorage.setItem('medp25',medp25.textContent);
+})
+btn26.addEventListener('click',function(){
+    btn26.textContent="Cart Added";
+    btn26.style.background="red";
+    let img26=document.getElementById('img26');
+    let med26=document.getElementById('med26');
+    let medp1=document.getElementById('medp26');
+    localStorage.setItem('img26',img26.src);
+    localStorage.setItem('med26',med26.textContent);
+    localStorage.setItem('medp26',medp26.textContent);
+})
+btn27.addEventListener('click',function(){
+    btn27.textContent="Cart Added";
+    btn27.style.background="red";
+    let img27=document.getElementById('img27');
+    let med27=document.getElementById('med27');
+    let medp27=document.getElementById('medp27');
+    localStorage.setItem('img27',img27.src);
+    localStorage.setItem('med27',med27.textContent);
+    localStorage.setItem('medp27',medp27.textContent);
+})
+btn28.addEventListener('click',function(){
+    btn28.textContent="Cart Added";
+    btn28.style.background="red";
+    let img28=document.getElementById('img28');
+    let med28=document.getElementById('med28');
+    let medp28=document.getElementById('medp28');
+    localStorage.setItem('img28',img28.src);
+    localStorage.setItem('med28',med28.textContent);
+    localStorage.setItem('medp28',medp28.textContent);
+})
+btn29.addEventListener('click',function(){
+    btn29.textContent="Cart Added";
+    btn29.style.background="red";
+    let img29=document.getElementById('img29');
+    let med29=document.getElementById('med29');
+    let medp29=document.getElementById('medp29');
+    localStorage.setItem('img29',img29.src);
+    localStorage.setItem('med29',med29.textContent);
+    localStorage.setItem('medp29',medp29.textContent);
+})
+btn30.addEventListener('click',function(){
+    btn30.textContent="Cart Added";
+    btn30.style.background="red";
+    let img30=document.getElementById('img30');
+    let med30=document.getElementById('med30');
+    let medp30=document.getElementById('medp30');
+    localStorage.setItem('img30',img30.src);
+    localStorage.setItem('med30',med30.textContent);
+    localStorage.setItem('medp30',medp30.textContent);
+})
---
+++ script2.js
@@ -0,0 +1,44 @@
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img1')+'><h2 class="medname">'+ localStorage.getItem('med1')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp1') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img2')+'><h2 class="medname">'+ localStorage.getItem('med2')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp2') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img3')+'><h2 class="medname">'+ localStorage.getItem('med3')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp3') +'</h2></div>';
+
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img4')+'><h2 class="medname">'+ localStorage.getItem('med4')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp4') +'</h2></div>';
+
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img5')+'><h2 class="medname">'+ localStorage.getItem('med5')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp5') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img6')+'><h2 class="medname">'+ localStorage.getItem('med6')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp6') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img7')+'><h2 class="medname">'+ localStorage.getItem('med7')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp7') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img8')+'><h2 class="medname">'+ localStorage.getItem('med8')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp8') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img9')+'><h2 class="medname">'+ localStorage.getItem('med9')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp9') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img10')+'><h2 class="medname">'+ localStorage.getItem('med10')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp10') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img11')+'><h2 class="medname">'+ localStorage.getItem('med11')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp11') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img12')+'><h2 class="medname">'+ localStorage.getItem('med12')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp12') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img13')+'><h2 class="medname">'+ localStorage.getItem('med13')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp13') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img14')+'><h2 class="medname">'+ localStorage.getItem('med14')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp14') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img15')+'><h2 class="medname">'+ localStorage.getItem('med15')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp15') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img16')+'><h2 class="medname">'+ localStorage.getItem('med16')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp16') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img17')+'><h2 class="medname">'+ localStorage.getItem('med17')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp17') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img18')+'><h2 class="medname">'+ localStorage.getItem('med18')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp18') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img19')+'><h2 class="medname">'+ localStorage.getItem('med19')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp19') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img20')+'><h2 class="medname">'+ localStorage.getItem('med20')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp20') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img21')+'><h2 class="medname">'+ localStorage.getItem('med21')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp21') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img22')+'><h2 class="medname">'+ localStorage.getItem('med22')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp22') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img23')+'><h2 class="medname">'+ localStorage.getItem('med23')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp23') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img24')+'><h2 class="medname">'+ localStorage.getItem('med24')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp24') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img25')+'><h2 class="medname">'+ localStorage.getItem('med25')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp25') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img26')+'><h2 class="medname">'+ localStorage.getItem('med26')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp26') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img27')+'><h2 class="medname">'+ localStorage.getItem('med27')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp27') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img28')+'><h2 class="medname">'+ localStorage.getItem('med28')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp28') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img29')+'><h2 class="medname">'+ localStorage.getItem('med29')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp29') +'</h2></div>';
+document.body.innerHTML += '<div class="elements"><img class="myimg" src='+localStorage.getItem('img30')+'><h2 class="medname">'+ localStorage.getItem('med30')+'</h2><h2 class="prices">Price: &#8377;'+localStorage.getItem('medp30') +'</h2></div>';
+
+let mytext=document.querySelector('.newtext');
+let medname=document.getElementsByClassName('medname');
+// console.log(medname);
+
+
+// mytext.innerHTML='hello';
+let checkout=document.querySelector('.checkout');
+console.log(checkout.textContent)
+checkout.addEventListener('click',function(){
+    mytext.innerHTML='Thank you for shopping with us, your cart has added successfully';
+})
